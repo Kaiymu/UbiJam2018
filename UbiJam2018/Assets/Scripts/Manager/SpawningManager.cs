b1cbc055d9934459bfb6135867cb1c07c2277649
Florian Dutronc <florian.dutronc@ubisoft.com>
2018-12-17 16:55:59 +0100

    [CODE] - Adding Spawn functionality

--- UbiJam2018/Assets/Scripts/Manager/SpawningManager.cs
+++ UbiJam2018/Assets/Scripts/Manager/SpawningManager.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class SpawningManager : MonoBehaviour {
 
+    [Header("Spawning")]
     public GameObject spawningZone;
-    public GameObject[] animals;
+    public Animal[] animals;
     public int initialNumberOfAnimals;
-    public int spawningIntervalInSec;
+    public float spawningIntervalInSec;
+    public float startSpawningTimeInSec;
+
+    void Spawn()
+    {
+        GameObject randomAnimal = GetRandomAnimal();
+        //Debug.Log(randomAnimal.name);
+        Vector2 position = GetRandomPosition();
+        //Debug.Log(position);
+        Instantiate(randomAnimal, position, Quaternion.identity);
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        return new Vector2(Random.value * spawningZone.transform.position.x, Random.value * spawningZone.transform.position.y);
+    }
+
+    GameObject GetRandomAnimal()
+    {
+        float total = 0;
+
+        foreach(Animal a in animals)
+        {
+            total += a.probability;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i = 0; i < animals.Length; i++)
+        {
+            if (randomPoint < animals[i].probability)
+            {
+                return animals[i].gameObject;
+            }
+            else {
+                randomPoint -= animals[i].probability;
+            }
+        }
+        return animals[animals.Length - 1].gameObject;
+    }
 
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < initialNumberOfAnimals; i++)
+        for (int i = 0; i < initialNumberOfAnimals - 1; i++)
         {
-
+            Spawn();
         }
+        InvokeRepeating("Spawn", startSpawningTimeInSec, spawningIntervalInSec);
     }
 
     // Update is called once per frame
